44da1690e99eb62c815c5bd21bc6f23636c6ffe9
praveenkumar09 <tpkprav@gmail.com>
2024-12-07 08:55:48 +0800

    Initial commit for trip advisor

---
+++ src/test/java/com/tpk/trip_advisor/TripAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpk.trip_advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
